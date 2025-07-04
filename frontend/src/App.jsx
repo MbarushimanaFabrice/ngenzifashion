32c91f84bfc6bb08b521d85d0283063ce824e7d3
mbarushimanafabrice <123445030+MbarushimanaFabrice@users.noreply.github.com>
2025-07-04 22:24:55 +0300

    Set up project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contect from './pages/Contect'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Order from './pages/Order'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+
+
+
+
+const App = () => {
   return (
-    <div className=' text-3xl'>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+          <Route path='/' element= {<Home/>} />
+          <Route path='/collection' element={<Collection/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<Order/>}/>
+          <Route path='/placeOrder' element={<PlaceOrder/>}/>
+          <Route path='/product/:productId' element={<Product/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/contect' element={<Contect/>}/>
+          <Route path='/about' element={<About/>}/>
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
